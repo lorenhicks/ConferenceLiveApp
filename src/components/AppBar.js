5309354a36125f05021c9ce059a10b80deffc430
Loren Hicks <lorenhicks@gmail.com>
2023-12-28 19:23:33 -0500

    Optimizing UI

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,7 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -22,9 +21,8 @@ export default function Appbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Conference Hall A
+            Conference Live 
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/components/Attendee.js
+++ src/components/Attendee.js
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 
 export default function Attendee() {
     const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' }
+    const paperStyleSpeaker = { padding: '50px 20px', width: 500, margin: '20px auto' }
     const headerStyle = { color: "#005EB8", "text-shadow": "-.5px .5px 1px rgba(20, 20, 20, 0.5)" }
     const [name, setName] = useState('')
     const [company, setCompany] = useState('')
@@ -21,7 +22,7 @@ export default function Attendee() {
             headers: { "Content-type": "application/json" },
             body: JSON.stringify(attendee)
         }).then(() => {
-            console.log('Speaker has checked in to Conference Hall A.')
+            console.log(JSON.stringify(attendee.name) + ' from ' + JSON.stringify(attendee.company) + ' has arrived.')
         })
     }
 
@@ -38,7 +39,7 @@ export default function Attendee() {
     return (
         <Container>
             <Paper elevation={3} style={paperStyle}>
-                <h1 style={headerStyle}>Check-in to Conference Hall A</h1>
+                <h1 style={headerStyle}>Check-In</h1>
                 <Box
                     component="form"
                     sx={{
@@ -47,18 +48,18 @@ export default function Attendee() {
                     noValidate
                     autoComplete="off"
                 >
-                    <TextField id="outlined-basic" label="Attendee Name" variant="outlined" fullWidth value={name} onChange={(e) => setName(e.target.value)} />
-                    <TextField id="outlined-basic" style={{ marginTop: "10px", marginBottom: "10px" }} label="Attendee Company" variant="outlined" value={company} onChange={(e) => setCompany(e.target.value)} fullWidth />
+                    <TextField id="outlined-basic" label="Speaker Name" variant="outlined" fullWidth value={name} onChange={(e) => setName(e.target.value)} />
+                    <TextField id="outlined-basic" style={{ marginTop: "10px", marginBottom: "10px" }} label="Speaker Company" variant="outlined" value={company} onChange={(e) => setCompany(e.target.value)} fullWidth />
                     <Button onClick={handleClick} color="blue" style={{ color: "white", background: "#005EB8" }}>Check-In</Button>
 
                 </Box>
-                <h1>Speakers</h1>
-                <Paper elevation={3} style={paperStyle}>
+                <h2>Arrivals</h2>
+                <Paper elevation={3} style={paperStyleSpeaker}>
                     {attendees.map(attendee => (
                         <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={attendee.id}>
-                            Employee ID: {attendee.id} <br />
-                            Name: {attendee.name} <br />
-                            Company: {attendee.company}
+                            Conference Hall: {attendee.id} <br />
+                            Speaker: {attendee.name} <br />
+                            Company: {attendee.company} <br />
                         </Paper>
                     ))}
                 </Paper>
